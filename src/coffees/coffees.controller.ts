db8c6e90abf62e2cc7be6e8ac238aa961cab34c3
marcinwojciechowskivi <marcin.wojciechowski@vistair.com>
2022-10-26 12:00:04 +0100

    patch and deleted

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('coffees')
 export class CoffeesController {
     @Get('flavours')
-    findAll() {
-        return `all coffees`;
+    findAll(@Res() response: Response) {
+        response.status(200).send(`all coffees from response object`);
     }
 
     @Get('flavours/:id')
@@ -13,12 +14,23 @@ export class CoffeesController {
     }
 
     @Post('flavours')
-    createFlavours(@Body('name') body, @Body('json') jsons) {
+    createFlavours(@Body('name') body: any, @Body('json') jsons: string) {
         return jsons;
     }
 
     @Post()
-    create(@Body() body) {
+    @HttpCode(HttpStatus.GONE)
+    create(@Body() body: any) {
         return body;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body: any) {
+        return `this will update ${id}`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `this will delete ${id}`;
     }
 }
